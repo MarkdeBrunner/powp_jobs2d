6776669981bde8092e6073df929d381edc4d94f5
Mark deBrunner <246325@edu.p.lodz.pl>
2025-11-29 11:39:47 +0100

    Refactor DrawPanelControllerAdapter to extend DrawerFeature and update drawing logic to single window display

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
@@ -1,14 +1,14 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawPanelControllerAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawPanelControllerAdapter extends DrawerFeature implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	public DrawPanelControllerAdapter() {
@@ -26,8 +26,8 @@ public class DrawPanelControllerAdapter extends DrawPanelController implements J
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
-		drawLine(line);
+		
+		getDrawerController().drawLine(line);
 		setPosition(x, y);
 	}
 
